4ae4c64349ce0cf7b1defea96852838fba7e681f
Huyen059 <samihuyen059@gmail.com>
2020-08-18 16:55:29 +0200

    use make:registration to create a registration page, in UserAuthentication, remember to specify a page to display after login

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -96,7 +96,7 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('test'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
